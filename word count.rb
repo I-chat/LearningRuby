32e522868bf7b680e047de64f8980a891a533b8e
Ichiato Ikikin <ichiatoikikin@gmail.com>
2015-05-12 11:47:06 +0100

    Initial commit. Added a new function

---
+++ word count.rb
@@ -0,0 +1,12 @@
+def frequency()
+	puts "Text Please: "
+	text = gets.chomp
+
+	words = text.split(" ")
+	frequencies = Hash.new(0)
+	words.each { |word| frequencies[word] += 1 }
+	frequencies = frequencies.sort_by {|a, b| b }
+	frequencies.reverse!
+	frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
+end
+frequency
